31d9928f0e005b16f45cfd01a9bff0972019109b
ChenSun-YP <mrsunchen0110@gmail.com>
2022-02-11 16:44:46 -0500

    feature pool formulation

--- util.py
+++ util.py
@@ -33,24 +33,29 @@
 #     def display(self):
 #         print("Note", self.channel, self.note, self.velocity, self.tick)
 
-class feature_pool:
-  def __init__(self):
-    self.feature_pool = []
-  def __init__(self, pool):
+class Feature_pool:
+
+  def __init__(self, pool=[]):
     self.feature_pool = pool
 
-  def new_feature(self, note, time):
+  def new_feature(self, notes):
+    '''
+    if it exists add to a feature if it not exist add it to a new feature object
+    :param notes:  a list of note objects 
+    :return: 
+    '''
+    name = [note.note for note in notes]
     # if note exist,add time to it or create a new note
     for item in self.feature_pool:
-      if item.note == note:
-        return item.add_time(time)
-    # not exist , new note 
-    new_feature = feature(note, time)
+      if item.name == name:
+        return item.add_phenotype(notes)
+    # not exist , new note
+    new_feature = Feature(notes)
     self.feature_pool.append(new_feature)
 
   def show_pool(self):
     for item in self.feature_pool:
-      print('feature:', item.note, "  time:", item.time, 'count:', len(item.time))
+      print('feature:', item.name, 'count:', item.count)
 
   def give_pool(self, min_count):
     pool_list = []
@@ -59,26 +64,27 @@ class feature_pool:
         pool_list.append(item)
     return pool_list
 
-
-class feature:
+class Feature:
   def __init__(self, notes):
     '''
     note is a list of note objects
     :param notes: 
     '''
-    self.name = (note.note for note in notes)
+    self.name = [note.note for note in notes]
     self.notes = notes
+    self.phenotypes  = [notes]
+    # self.duration =[[note.duration for note in notes]]
     # self.duration = notes
-    # self.time = [time]
-    # self.count = 1
+    self.count = 1
 
   def add_phenotype(self, notes):
-    # if exist add count if new add  to list
-    time = (note.time for note in notes)
-    if time not in self.time:
-      self.time.append(time)
+    # add count if new add  to list if exist add to list
+    duration = [note.duration for note in notes]
+    if duration not in self.phenotypes:
+      self.count += 1
+      return self.phenotypes.append(notes)
     self.count += 1
-    return self.time.append(time)
+    return self.phenotypes.append(notes)
 
   def get_time(self, time):
     return time in self.time
@@ -86,17 +92,16 @@ class feature:
   def get_note(self):
     return self.note
 
-
-class note:
+class Note:
   def __init__(self):
     self.channel = 0
     self.note = [-1]
     self.velocity = 0
-    self.time = 0
+    self.start_time = 0
     self.duration = 0
-    self.type = True
 
-  def __init__(self, channel, note, velocity, time, duration,note_type=True):
+
+  def __init__(self, channel, note, velocity, start_time, duration):
     '''
     with argument
     :param channel: 
@@ -108,29 +113,77 @@ class note:
     self.channel = channel
     self.note = note
     self.velocity = velocity
-    self.time = time
+    self.start_time = start_time
     self.duration = duration
-
-    self.type = note_type
-
-  def set_channel(self, channel):
-    self.channel = channel
-
-  def set_note(self, note):
-    self.note = note
-
-  def set_velocity(self, velocity):
-    self.velocity = velocity
-
-  def set_tick(self, tick):
-    self.tick = tick
-
-  def set_type(self, type):
-    self.type = type
 
   def display(self):
     print("Note", self.channel, self.note, self.velocity, self.tick)
 
+class Track:
+    def __init__(self):
+      self.note_list = []
+      self.size = 0
+
+    def note_append(self, new_note):
+      self.note_list.append(new_note)
+      self.size += 1
+
+class Music:
+    def __init__(self):
+      self.track_list = []
+      self.ticks_per_beat = 100
+
+    def set_ticks_per_beat(self, ticks_per_beat):
+      self.ticks_per_beat = ticks_per_beat
+
+    def append_track(self, new_track):
+      self.track_list.append(new_track)
+
+    def display(self):
+      for i in range(len(self.track_list)):
+        print("Track:", i)
+        print("Note      :", end="")
+        for j in range(len(self.track_list[i].note_list)):
+          print(self.track_list[i].note_list[j].note, end='\t')
+        print("")
+        print("Start Time:", end="")
+        for j in range(len(self.track_list[i].note_list)):
+          print(self.track_list[i].note_list[j].start_time, end='\t')
+        print("")
+        print("Duration  :", end="")
+        for j in range(len(self.track_list[i].note_list)):
+          print(self.track_list[i].note_list[j].duration, end='\t')
+        print("")
+
+        print("")
+
+    # TODO
+    """
+    def save_midi(self,save_path = 'new_song.mid'):
+        mid = MidiFile()
+        for track_index in range(len(self.track_list)):
+
+            track = MidiTrack()
+            mid.tracks.append(track)
+            # track.append(Message('program_change', program=12, time=0))
+            for note_index in range(self.track_list[track_index].size):
+                # print("有track",track_index)
+                note = self.track_list[track_index].note_list[note_index]
+                velocity = note.velocity
+                note_note = note.note
+                tick = note.tick
+                channel = note.channel
+                if note.type:
+                    for i in note_note:
+                        track.append(Message('note_on', channel=channel, note=i, velocity=velocity, time=tick))
+                else:
+                    for i in note_note:
+                        track.append(Message('note_off', channel=channel, note=i, velocity=velocity, time=tick))
+        track.append(MetaMessage('end_of_track', time=0))
+        mid.ticks_per_beat=self.ticks_per_beat
+        mid.save(save_path)
+        """
+
+
 if __name__ == "__main__":
-    note1 = Note(13,40,80,100,True)
-    note1.display()
+    pass
