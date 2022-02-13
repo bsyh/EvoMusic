eab13c7adf38149f4769f729048b4f1082addb5d
ChenSun-YP <mrsunchen0110@gmail.com>
2022-02-13 17:10:51 -0500

    add rules

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from mido import Message
 
 
 
+# provide choice
 
 from feature_extraction import read_to_notes,containsPattern,compose
 from util import Feature,Feature_pool
@@ -24,12 +25,15 @@ from util import Feature,Feature_pool
 #read input 1
 pop_num = 5
 length =30
-source1,tick1 = read_to_notes('2.mid')
+source1 = read_to_notes('12barblues_ms.mid')
+
 #read input 2
-source2,tick2 = read_to_notes('2.mid')
+source2 = read_to_notes('2.mid')
 print(source2)
+
 #init pool
 feature_pool = Feature_pool()
+
 #extract featurse
 
 containsPattern(feature_pool, source1,tick1)
@@ -45,11 +49,12 @@ for i in range(pop_num):
 
 population[0].display()
 
+population[0].ticks_per_beat = 480
 save_path = "my_music.mid"
 population[0].save_midi(save_path)
 mid2 = MidiFile(save_path)
-# from midi_visualize import midi_visualize
-# midi_visualize(save_path)
+from midi_visualize import midi_visualize
+midi_visualize(save_path)
 
 
 #loop of evoluation
@@ -57,12 +62,46 @@ mid2 = MidiFile(save_path)
 #parent selection 
 
 #crossover
-
+population[0].display()
+population[1].display()
+print("Cross-over:")
+one_pt_crossover(population[0],population[1])
 #mutation
+population[0].display()
+population[1].display()
+print("Reverse mutated:")
+reverse_mutation(population[0],track_index=0,feature_index=0)
+reverse_mutation(population[1],track_index=0,feature_index=0)
+population[0].display()
+population[1].display()
 
+
+
+inloop = True
+while True:
+  # output 1，2 TODO switch to evaluated best as competcotr
+  choice1 = music.save_midi(population[0])
+  choice2 = music.save_midi(population[1])
+
+  # receive choice
+  desicion = input('I prefer___,anything else for roll')  #reroll to provide another set of choice
+
+  if desicion == 1:
+    #give 1 more weighs
+    print('1 is better')
+    break
+  elif desicion == 2:
+    #give 2more weighs
+    print('2 is better')
+
+    break
+
+#modify evaulation
 
 
 
 #end loop
-#output
+
+
+#output final result
 
