69a62f37dd7493ff6aea7a65dacfce093f5ff95c
ChenSun-YP <mrsunchen0110@gmail.com>
2022-02-13 16:37:32 -0500

    add rules

--- main.py
+++ main.py
@@ -21,22 +21,18 @@ from mido import Message
 from feature_extraction import read_to_notes,containsPattern,compose
 from util import Feature,Feature_pool
 
-
-
-
 #read input 1
 pop_num = 5
 length =30
-source1 = read_to_notes('12barblues_ms.mid')
+source1,tick1 = read_to_notes('2.mid')
 #read input 2
-source2 = read_to_notes('2.mid')
-
+source2,tick2 = read_to_notes('2.mid')
 print(source2)
 #init pool
 feature_pool = Feature_pool()
 #extract featurse
 
-containsPattern(feature_pool, source1)
+containsPattern(feature_pool, source1,tick1)
 feature_pool.show_pool()
 
 
@@ -49,12 +45,11 @@ for i in range(pop_num):
 
 population[0].display()
 
-population[0].ticks_per_beat = 480
 save_path = "my_music.mid"
 population[0].save_midi(save_path)
 mid2 = MidiFile(save_path)
-from midi_visualize import midi_visualize
-midi_visualize(save_path)
+# from midi_visualize import midi_visualize
+# midi_visualize(save_path)
 
 
 #loop of evoluation
@@ -62,18 +57,9 @@ midi_visualize(save_path)
 #parent selection 
 
 #crossover
-population[0].display()
-population[1].display()
-print("Cross-over:")
-one_pt_crossover(population[0],population[1])
+
 #mutation
-population[0].display()
-population[1].display()
-print("Reverse mutated:")
-reverse_mutation(population[0],track_index=0,feature_index=0)
-reverse_mutation(population[1],track_index=0,feature_index=0)
-population[0].display()
-population[1].display()
+
 
 
 
