e8962a0d78f9d8e0f9212033373224e7d4dd4b57
shouyuehu <17sh61@queensu.ca>
2022-02-17 20:46:38 -0500

    fix tick_per_beat import

--- util.py
+++ util.py
@@ -173,7 +173,7 @@ class Music:
                 # print("有track",track_index)
                 for note_list in self.track_list[track_index].feature_list[feature_index]:
                     for note in note_list:
-                        velocity = note.velocity  #TODO shold be feature.velcoty
+                        velocity = 127  #TODO shold be feature.velcoty
                         note_note = note.note
                         start_time = note.start_time
                         duration = note.duration
