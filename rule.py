48cf4c792c0309f450238b8e320c534120ef93f7
ChenSun-YP <mrsunchen0110@gmail.com>
2022-03-17 18:04:54 -0400

    added rule

---
+++ rule.py
@@ -0,0 +1,37 @@
+#test rule    
+
+
+from EvoMusic.util import Music
+
+def first_rule(music):
+  if music.fitness ==0:
+    return 1
+  else:
+    return 
+  
+def micro_pitch_order(music1,music2):
+  '''
+  TODO bruce
+  一个feature里面note的相对关系,音高权重1，长度权重0.5
+  :param music: 
+  :return: 
+  '''
+  return 0
+
+def macro_pitch_order(music1,musci2):
+  '''
+  TODO bruce
+  一个feature里面note的相对关系,音高权重1，长度权重0.5
+  :param music: 
+  :return: 
+  '''
+  return 0
+
+def frequcncy(music):
+  '''
+  按照main词频获取fitness
+  :param music: 
+  :return: 
+  '''
+  return 0
+
