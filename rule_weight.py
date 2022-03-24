639142c710fd224d5048c951cd073ac45f4ba033
shouyuehu <17sh61@queensu.ca>
2022-03-23 22:30:41 -0400

    add rule_weight.py as setter and getter of weights of rule

--- rule_weight.py
+++ rule_weight.py
@@ -4,12 +4,21 @@
 
 
 def set_weight(list):
+    '''
+
+    :param list: a list of float to be saved
+    :return:
+    '''
     rule_weight = open("rule_weight.txt","w")
     for value in list:
         rule_weight.write(str(value)+"\n")
     rule_weight.close()
 
 def read_weight():
+    '''
+
+    :return: the list of floating point saved in rule_weight.txt
+    '''
     with open("rule_weight.txt","r") as f:
         weight = [float(line.strip()) for line in f]
         f.close()
