063182ff5f3200dcf39c73c5d0ea06bd34f8109d
shouyuehu <17sh61@queensu.ca>
2022-03-24 19:38:09 -0400

    api.py is no longer needed, as main.py take it place

--- main.py
+++ main.py
@@ -138,89 +138,74 @@ def loop(choice="r"):
   :return: new population,final_product
   '''
   global counter, gen
-  inloop = True
 
 
   with open('population.list', 'rb') as population_file:
     population = pickle.load(population_file)
 
 
-  while inloop:
-    #TODO
-    # population[0].display()
-    # population[1].display()
-    one_pt_crossover(population[0], population[1])
-    # mutation
-    # population[0].display()
-    # population[1].display()
-    reverse_mutation(population[0], track_index=0, feature_index=0)
-    # reverse_mutation(population[1], track_index=0, feature_index=0)
-    # population[0].display()
-    # population[1].display()
-  
-  
-    if gen%50 == 0:
-      weight = read_weight()
-      #user selection
-      # output 1，2 TODO switch to evaluated best as competcotr
-      a,b= feedmax(population)
-      counter +=1
-      a.save_midi("choices/my_music"+str(counter)+".mid")
-      counter +=1
-      b.save_midi("choices/my_music."+str(counter)+"mid")
-      
-      # receive choice
-      desicion = choice #reroll to provide another set of choice
-      
-      if desicion == '1':
-        #give 1 more weighs
-        print('1 is better')
-        desicion =a
-        print(a.fitness)
-  
-        fitness_list_1 = evaluation(population, desicion, weight)
-  
-  
-      elif desicion == '2':
-        #give 2more weighs
-        print('2 is better')
-        desicion =b
-        print(b.fitness)
-  
-        fitness_list_1 = evaluation(population, desicion, weight)
-  
-  
-      elif desicion == 's':
-        #stop loop
-        # output final result
-        # modify evaulation
-        fitness_list_1 = evaluation(population, desicion, weight)
-        # weight = [1, 0, 1]
-        # fitness_list_2=evaluation(population,org,weight)
-        fitness_list = []
-        for i in range(len(fitness_list_1)):
-          # fitness_list.append(fitness_list_1[i]+fitness_list_2[i])
-          fitness_list.append(fitness_list_1[i])
-        final, secondbest = feedmax(population)
-        final.save_midi("choices/final.mid")
-        inloop = False
-        break
-        
-      elif desicion == 'r':
-        #roll again
-    
-        # set weight to zeros, random evolve
-        old_weight = weight
-        set_weight([0,0,0])
-    
-        loop("1")
+  weight = read_weight()
+  #user selection
+  # output 1，2 TODO switch to evaluated best as competcotr
+  a,b= feedmax(population)
+  counter +=1
+  a.save_midi("choices/my_music"+str(counter)+".mid")
+  counter +=1
+  b.save_midi("choices/my_music."+str(counter)+"mid")
 
-        set_weight(old_weight)
-    
-      else:
-        print('wrong input')
-  with open('population.list', 'wb') as population_file:
-    pickle.dump(population, population_file)
+  # receive choice
+  desicion = choice #reroll to provide another set of choice
+
+  if desicion == '1':
+    #give 1 more weighs
+    print('1 is better')
+    desicion =a
+    print(a.fitness)
+
+    fitness_list_1 = evaluation(population, desicion, weight)
+
+
+  elif desicion == '2':
+    #give 2more weighs
+    print('2 is better')
+    desicion =b
+    print(b.fitness)
+
+    fitness_list_1 = evaluation(population, desicion, weight)
+
+
+  elif desicion == 's':
+    #stop loop
+    # output final result
+    # modify evaulation
+    fitness_list_1 = evaluation(population, desicion, weight)
+    # weight = [1, 0, 1]
+    # fitness_list_2=evaluation(population,org,weight)
+    fitness_list = []
+    for i in range(len(fitness_list_1)):
+      # fitness_list.append(fitness_list_1[i]+fitness_list_2[i])
+      fitness_list.append(fitness_list_1[i])
+    final, secondbest = feedmax(population)
+    final.save_midi("choices/final.mid")
+
+    return False
+
+
+  elif desicion == 'r':
+    #roll again
+
+    # set weight to zeros, random evolve
+    old_weight = weight
+    set_weight([0,0,0])
+
+    loop("1")
+
+    set_weight(old_weight)
+
+  else:
+    #do nothing
+    pass
+
         
         
     # modify fitness base on selection
@@ -241,9 +226,23 @@ def loop(choice="r"):
     gen +=1
     
     
+  for loop_count in range(100):
 
+
+    one_pt_crossover(population[0], population[1])
+    # mutation
+    # population[0].display()
+    # population[1].display()
+    reverse_mutation(population[0], track_index=0, feature_index=0)
+    # reverse_mutation(population[1], track_index=0, feature_index=0)
+    # population[0].display()
+    # population[1].display()
+
+
+  with open('population.list', 'wb') as population_file:
+    pickle.dump(population, population_file)
   
-  
+  return True
 
 
 
